c34a6e623e26cb294cff9822d0ee33e2bf341aa5
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-10-05 20:22:23 -0400

    trouble shooted hard hat but compiled youtube smart contract

--- web3_ytclone/hardhat.config.js
+++ web3_ytclone/hardhat.config.js
@@ -1,6 +1,40 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// to deploy, we need private key
+
+// module.exports = {
+//   defaultNetwork: "goerli",
+//   networks: {
+//     hardhat: {},
+//     goerli: {
+//       url: process.env.URL,
+//       accounts: process.env.PRIVATE_KEY,
+//     },
+//   },
+//   solidity: {
+//     version: "0.8.9",
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 200,
+//       },
+//     },
+//   },
+//   paths: {
+//     artifacts: "./artifacts",
+//   },
+// };
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.0",
+  defaultNetwork: "Goerli",
+  networks: {
+    hardhat: {},
+    Goerli: {
+      url: API_URL,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+  },
 };
