2e42a28776823217d106cd5aa5bacfe408bfd7e3
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-10-17 13:24:13 -0400

    cant resolve fs next

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import { LivepeerConfig } from "@livepeer/react";
+import { useCreateAsset } from "@livepeer/react";
 import LivePeerClient from "../livepeer";
+import saveToIPFS from "../utils/saveToIPFS";
 
 function MyApp({ Component, pageProps }) {
   return (
